7440566f34b14068461a06d84864b6376d6b9b77
AwesomeRoy <spamitietes@live.nl>
2014-01-12 03:57:59 +0100

    only works with /dashboard  but well It works in one way ! :D

--- models/dashboard_model.php
+++ models/dashboard_model.php
@@ -1,15 +1,15 @@
 <?php
 
-class Dashboard_Model {
+class Dashboard_Model extends Model {
 
     function __construct() {
         parent::__construct();
     }
     
     public function overview() {
-        $sth = $this->db->prepare("SELECT * FROM posts LEFT JOIN users ON post_by = user_id ORDER BY post_date DESC");
+        $sth = $this->db->prepare("SELECT * FROM posts LEFT JOIN users ON post_by = id ORDER BY post_date DESC");
         $sth->execute();
-        return $sth->fetchAll();
+        return $sth->fetchAll();  
     }
 }
 
--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -3,12 +3,19 @@
 		<div class="sixteen cell">
 			<div class="bg block">
 				<div class="title">
-					Hooray!!
+                                    Hooray!!
 				</div>
 				<div class="content">
-					You are now logged in, <?php echo Session::get('username'); ?>! <br />
-                                        We are really happy to see you here! <br />
-                                       <?php print_r($this->overview);  ?>  
+                                    You are now logged in, <?php echo Session::get('username'); ?>! <br />
+                                    We are really happy to see you here! <br />
+                            <br />
+                                    <?php 
+                                        //print_r($this->overview);
+                                        foreach($this->overview as $key => $value){
+                                            echo "This post is uploaded by '".$value['username']."': <br />
+                                                ".$value['post_content']."<br />".$value['post_date'];
+                                        }
+                                        ?>  <br />
 				</div>
 			</div>
 		</div>
